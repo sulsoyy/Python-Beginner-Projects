b6d4b51f164df5200e06c09b57831d12afeadc72
sulsoyy <soyeonjangsoy@gmail.com>
2023-06-22 22:38:20 +0900

    relocate files

--- Follow-up/Follow-up.py
+++ Follow-up/Follow-up.py
@@ -1,20 +1,53 @@
+import random
 
 
-# inserted words by a user
+# List of words that are inserted by a user
+
 words = []
 
 
+# List of initial_words
 
-def tutorial():
-    pass
+initial_words = ['Apple', 'Banana', 'Carrot', 'Durian', 'Eggplant', 'Figs', 'Grape', 'Hazelnuts', 'Icaco', 'Jalapeno', 'Kale', 'Lemon', 'Mango', 'Nectarine', 'Orange', 'Peanut', 'Quince', 'Raddish', 'Strawberry', 'Tomato', 'Ugli', 'Vanilla', 'Watermelon', 'Xoconostle', 'Yam', 'Zucchini']
+
+# Score
+
+score = 0
 
 
-print("Welcome to Follow-up game where you need to type a word that starts with a last letter of given word")
+# function
 
-print("Type your first word!")
-initial_word = input().capitalize()
-#words = words.append(initial_word)
-print(initial_word)
+def check_word_ending():
+    if word not in words:
+            word = input("Type a word that starts with " + word[-1].capitalize() + "\n").capitalize()
+            words.append(word)
+            score += 1
+
+    else:
+        word = input("Type a word that starts with " + word[-1].capitalize() + "\n").capitalize()
+
+
+
+
+# Game Instruction
+
+initial_word = random.choice(initial_words)
+words.append(initial_word)
+
+word = input("Type a word that starts with the last letter of " + initial_word + " - " + initial_word[-1].capitalize() + "\n").capitalize()
+
+while score <= score + 1:
+    if word not in words:
+            word = input("Type a word that starts with " + word[-1] + "\n")
+            words.append(word)
+            score += 1
+
+    elif word in words:
+        word = input("Type a word that starts with the last letter of " + initial_word + " - " + initial_word[-1].capitalize() + "\n").capitalize()
+
+    break
+
+
 
 
 
